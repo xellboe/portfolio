6bf986adc4ff13a09cecce529bdaa6c76f133a91
xellboe <nkorotkyi@gmail.com>
2023-07-18 17:51:47 +0300

    minset

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,5 @@
 "use client"
 import { useEffect, useState } from "react"
-import { useEffect } from "react/cjs/react.development"
 
 const getWindowWidth = () => {
   const { innerWidth: windowWidth } =
